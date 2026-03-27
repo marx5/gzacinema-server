182681109d6f30d9a73689cb283572a8858a2edc
Van Vu <vulv.nvgb@gmail.com>
2026-03-27 13:02:40 +0700

    redis fix connection

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,18 +1,15 @@
 const Redis = require('ioredis');
 
-const redis = new Redis({
-    host: process.env.REDIS_HOST || 'localhost',
-    port: process.env.REDIS_PORT || 6379,
-    password: process.env.REDIS_PASSWORD || null,
+const redis = new Redis(process.env.REDIS_URL, {
     tls: {}
-})
+});
 
 redis.on('connect', () => {
     console.log('Connected to Redis');
-})
+});
 
 redis.on('error', (err) => {
     console.error('Redis connection error:', err);
-})
+});
 
 module.exports = redis;
