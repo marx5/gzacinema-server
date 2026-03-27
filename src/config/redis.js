d28153b97019ad08be10d0b4278ae3ad39cc5f00
Van Vu <vulv.nvgb@gmail.com>
2026-03-27 12:53:10 +0700

    redis fix tls

--- src/config/redis.js
+++ src/config/redis.js
@@ -4,6 +4,7 @@ const redis = new Redis({
     host: process.env.REDIS_HOST || 'localhost',
     port: process.env.REDIS_PORT || 6379,
     password: process.env.REDIS_PASSWORD || null,
+    tls: {}
 })
 
 redis.on('connect', () => {
